1ca8ba6ae9754e257e661161779db1ea2ae6ed83
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-03-31 22:37:14 -0700

    Update README.md

---
+++ Peliculas/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Peliculas
+{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            try
+            {
+                Pelicula a = new Pelicula(textBox1.Text, Int16.Parse(textBox2.Text));
+                label1.Text = a.ToString();
+
+                Pelicula b = new Pelicula(textBox3.Text, Int16.Parse(textBox4.Text));
+                label2.Text = b.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Todos los campos deben tener un dato valido","Error");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e){
+            Application.Exit();
+        }
+    }
+    class Pelicula
+    {
+        private string titulo;
+        private Int16 año;
+        private string pais;
+        private string director;
+        public Pelicula(string titulo, Int16 año)
+        {
+            this.titulo = titulo;
+            this.año = año;
+        }
+        public override String ToString() => String.Format("La película {0} se estrenó el año {1}.", this.titulo, año);
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Programacion Orientada a Objeto (Visual)
 Paso de practicas a Visual  
+[Practica #2](./Peliculas/Form1.cs) Programa Básico de Peliculas  
 [Practica #7](./Suma_Domino/Form1.cs) Sumar puntos Domino (Sobrecarga operadores)  
 [Practica #8](./Duracion_Visual/Form1.cs) Sumar duraciones (Sobrecarga operadores)  
