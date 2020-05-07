9a2337139de2d93065dbf427f0028d0dd7c6a275
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-05-06 20:51:12 -0700

    Cambio de color de figuras

--- Figuras/Figura.cs
+++ Figuras/Figura.cs
@@ -2,16 +2,13 @@
 using System.Windows.Forms;
 
 namespace Figuras {
-    class Figura {
-
-    }
-    class Rectangulo {
-        Pen pluma;
-        Color tinta;
-        SolidBrush brocha;
-        int x, y;
-        int width,height;
-        public Rectangulo(int x, int y, int width, int height) {
+    abstract class Figura {
+        protected Pen pluma;
+        public Color tinta { get; set; }
+        public SolidBrush brocha { get; set; }
+        protected int x, y;
+        protected int width,height;
+        public Figura(int x, int y, int width, int height) {
             this.x = x;
             this.y = y;
             this.width = width;
@@ -20,10 +17,34 @@ namespace Figuras {
             pluma = new Pen(tinta);
             brocha = new SolidBrush(Color.Red);
         }
-        public void dibuja(Form lienzo) {
+        public virtual void dibuja(Form Lienzo) { }
+        /// <summary>
+        /// Determina si el punto dado se encuentra en la figura
+        /// </summary>
+        /// <param name="p_x">Ingresar la coordenada X del punto a conocer</param>
+        /// <param name="p_y">Ingresar la coordenada Y del punto a conocer</param>
+        /// <returns>Si el punto dado se encuentra en la figura</returns>
+        public bool Incluye(int p_x, int p_y) {
+            return (p_x >= this.x && p_x <= this.x + this.width) && (p_y >= this.y & p_y <= this.y + this.height);
+        }
+    }
+    class Rectangulo:Figura {
+        public Rectangulo(int x, int y, int width, int height):base(x,y,width,height) {
+        }
+        public override void dibuja(Form lienzo) {
             Graphics pintar = lienzo.CreateGraphics();
             pintar.FillRectangle(this.brocha, this.x, this.y, this.width, this.height);
             pintar.DrawRectangle(this.pluma, this.x, this.y, this.width, this.height);
+            pintar.Dispose();
+        }
+    }
+    class Elipse : Figura {
+        public Elipse(int x, int y, int width, int height) : base(x, y, width, height) {
+        }
+        public override void dibuja(Form lienzo) {
+            Graphics pintar = lienzo.CreateGraphics();
+            pintar.FillEllipse(this.brocha, this.x, this.y, this.width, this.height);
+            pintar.DrawEllipse(this.pluma, this.x, this.y, this.width, this.height);
         }
     }
 }
--- Figuras/Form1.cs
+++ Figuras/Form1.cs
@@ -3,36 +3,124 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Figuras {
+    enum EstadoActual { seleccionando, rectangulo, elipse }
     public partial class Form1 : Form {
         private Label etiqueta = new Label();
-        private List<Rectangulo> figs;
+        private List<Figura> figs;
+        EstadoActual estado= EstadoActual.seleccionando;
+        private bool dibujando=false;
+        private int temp_x;
+        private int temp_y;
+        private Button Elipse;
+        private Button Rectangulo;
+        private Button Seleccion;
+        
         public Form1() {
-            //InitializeComponent();
-            figs = new List<Rectangulo>();
+            figs = new List<Figura>();
+            this.Elipse = new Button();
+            this.Rectangulo = new Button();
+            this.Seleccion = new Button();
 
+            this.Elipse.Location = new Point(400,50);
+            this.Rectangulo.Location = new Point(300,50);
+            this.Seleccion.Location = new Point(200,50);
             this.etiqueta.Location = new Point(50, 50);
 
-            this.ClientSize = new Size(400,400);
+            this.Elipse.Text = "Elipse";
+            this.Rectangulo.Text = "Rectángulo";
+            this.Seleccion.Text = "Selecciona";
+
+            this.Elipse.TabIndex = 2;
+            this.Rectangulo.TabIndex = 1;
+            this.Seleccion.TabIndex = 0;
+
+            this.Elipse.Click += new EventHandler(Elipse_Click);
+            this.Rectangulo.Click+=new EventHandler(Rectangulo_Click);
+            this.Seleccion.Click+=new EventHandler(Seleccion_Click);
+
+            this.ClientSize = new Size(600,400);
             this.Controls.Add(etiqueta);
+            this.Controls.Add(this.Elipse);
+            this.Controls.Add(this.Rectangulo);
+            this.Controls.Add(this.Seleccion);
+
             this.MouseClick += new MouseEventHandler(Mouse_Click);
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            this.MouseUp += new MouseEventHandler(Form1_MouseUp);
+            this.MouseMove += new MouseEventHandler(Form1_MouseMove);
             this.Paint += new PaintEventHandler(repintar);
 
         }
         public void Mouse_Click(object sender, MouseEventArgs e) {
-            this.etiqueta.Text = String.Format("{0},{1}", e.X, e.Y);           
-            Rectangulo pintar = new Rectangulo(e.X,e.Y,20,20);
-            pintar.dibuja(this);
-            figs.Add(pintar);
+            this.etiqueta.Text = String.Format("{0},{1}", e.X, e.Y);
+            if(e.Button.Equals(MouseButtons.Left)&& this.estado.Equals(EstadoActual.seleccionando)) {
+                Figura seleccionada=null;
+                seleccionada = GetFigura(e.X, e.Y);
+                if (seleccionada != null) {
+                    seleccionada.brocha = new SolidBrush(Color.Blue);
+                }
+                    
+            }
         }
-        public void repintar(object sender, PaintEventArgs e) {
-            foreach (var cuadros in figs)
-                cuadros.dibuja(this);
+        private Figura GetFigura(int x,int y) {
+            foreach(Figura f in figs) {
+                if (f.Incluye(x, y))
+                    return f;
+            }
+            return null;
+        }
+        private void Seleccion_Click(object sender,EventArgs e) {
+            estado = EstadoActual.seleccionando;
+        }
+        private void Elipse_Click(object sender, EventArgs e) {
+            estado = EstadoActual.elipse;
+        }
+        private void Rectangulo_Click(object sender, EventArgs e) {
+            estado = EstadoActual.rectangulo;
+        }
+        private void Form1_MouseDown(object sender,MouseEventArgs e) {
+            this.temp_x = e.X;
+            this.temp_y = e.Y;
+            if (!dibujando & estado!=EstadoActual.seleccionando)
+                dibujando = true;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e) {
+            if (dibujando) {
+                Graphics pintar = this.CreateGraphics();
+                switch (estado) {
+                    case EstadoActual.rectangulo:
+                        pintar.DrawRectangle(new Pen(Color.Black), this.temp_x, this.temp_y, e.X - this.temp_x, e.Y - this.temp_y);
+                    break;
+                    case EstadoActual.elipse:
+                        pintar.DrawEllipse(new Pen(Color.Black), this.temp_x, this.temp_y, e.X - this.temp_x, e.Y - this.temp_y);
+                    break;
+            }
+                
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e) {
+            if (dibujando) {
+                Figura f=null;
+                switch (estado) {
+                    case EstadoActual.rectangulo:
+                        f= new Rectangulo(this.temp_x,this.temp_y, Math.Abs(e.X-this.temp_x),Math.Abs(e.Y-this.temp_y));
+                    break;
+                    case EstadoActual.elipse:
+                        f=new Elipse(this.temp_x,this.temp_y,e.X-this.temp_x,e.Y-this.temp_y);
+                    break;
+                }
+                f.dibuja(this);
+                figs.Add(f);
+                this.dibujando = false;
+            }
+        }
+        private void repintar(object sender, PaintEventArgs e) {
+            foreach (Figura f in figs)
+                f.dibuja(this);
+
         }
     }
 }
