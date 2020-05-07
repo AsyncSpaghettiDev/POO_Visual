6ef25cb534474f2bc6411d23a91370fc7b6ef004
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-05-06 20:53:27 -0700

    Implementacionde tablero en ventana

--- Celdas/Juego.cs
+++ Celdas/Juego.cs
@@ -11,6 +11,18 @@ using System.Windows.Forms;
 
 namespace Celdas {
     public partial class Juego : Form {
-        
+        Button pintar;
+        Tablero juego;
+        public Juego() {
+            Funciones.Diseno(this, 500, 500, "Clon Triple Town","logo.ico");
+            juego = new Tablero();
+            this.pintar = new Button();
+            this.pintar.Location = new Point(350,30);
+            this.pintar.Click += new EventHandler(presionar);
+            this.Controls.Add(pintar);
+        }
+        public void presionar(object sender,EventArgs e) {
+            juego.pintar_tablero(this);
+        }
     }
 }
