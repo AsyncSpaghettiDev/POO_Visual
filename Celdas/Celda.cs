a715ff3e57862230a8527a558d1cdfd9a27489b1
JonathanMov <60447812+JonathanMov@users.noreply.github.com>
2020-05-06 20:52:43 -0700

    Creacion de cledas

---
+++ Celdas/Celda.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Celdas {
+    public enum Type { 
+        empty, 
+        grass, 
+        bush, 
+        tree,
+        house 
+    }
+    class Celda {
+        Type estado;
+        int size=50;
+        int row,column;
+        int x,y;
+        List<Bitmap> _objeto = new List<Bitmap>();
+        public Celda(int row,int column, Type estado) {
+            
+            this.estado = estado;
+            this.row = row;
+            this.column = column;
+            this.x = row * size;
+            this.y = column * size;
+            _objeto.Add(new Bitmap("../../img/path.bmp"));
+            _objeto.Add(new Bitmap("../../img/Grass.png"));
+            _objeto.Add(new Bitmap("../../img/Bush.png"));
+            _objeto.Add(new Bitmap("../../img/Tree.png"));
+            _objeto.Add(new Bitmap("../../img/House.png"));
+        }
+        public void Dibuja(Form Lienzo) {
+            Graphics imagen = Lienzo.CreateGraphics();
+            try {
+                if(estado!=Type.empty)
+                    imagen.DrawImage(new Bitmap("../../img/bg.bmp"), size * row, size * column, size, size);
+                imagen.DrawImage(_objeto[(int)this.estado], size * row, size * column, size, size);
+            }
+            catch (Exception e) {
+                MessageBox.Show(e.Message+e.TargetSite);
+            }
+        }
+    }
+}
---
+++ Celdas/Funciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+
+/// <summary>
+    /// Configuraciones genericas.
+    /// </summary>
+public class Funciones {
+    /// <summary>
+        /// <para> Establece el tamaño de la ventana, y su icono
+        /// Ademas de dejar la ventana estática para evitar su redimension. </para>
+        /// </summary>
+        /// <param name="Lienzo"> Pasa la form actual, se recomienda usar this </param>
+        /// <param name="x">Ingresa la dimension X deseada de la ventana </param>
+        /// <param name="y">Ingresa la dimension Y deseada de la ventana </param>
+        /// <param name="Titulo">Ingresa el nombre deseado de la ventana</param>
+        /// <param name="nombre">Ingresa el nombre del icono de la ventana, nota: poner en bin/Debug/</param>
+    public static void Diseno(Form Lienzo,int x,int y,String Titulo,String nombre) {
+        Lienzo.Size = new Size(x, y);
+        Lienzo.Icon = new Icon("./"+nombre);
+        Lienzo.Text = Titulo;
+
+        Lienzo.FormBorderStyle = FormBorderStyle.Fixed3D;
+        Lienzo.MaximizeBox = false;
+        Lienzo.MinimizeBox = false;
+
+        Lienzo.FormClosed += new FormClosedEventHandler(cerrar);
+    }
+    private static void cerrar(object sender, EventArgs e) => Application.Exit();
+}
